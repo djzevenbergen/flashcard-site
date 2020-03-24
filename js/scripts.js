415c2830526bdbd3eefd2d9785345a7046cbd1b8
djzevenbergen <djzevenbergen@gmail.com>
2020-03-24 15:31:08 -0700

    Add clickable flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     $("body").toggleClass("green-background");
     $("p").toggleClass("initially-hidden");
     $("h1").toggleClass("initially-hidden");
+  });
+  $(".panel-body").click(function() {
+        $(this).children("p").toggleClass("initially-hidden");
+        $(this).children("h1").toggleClass("initially-hidden");      
+  });
 
-  })
 });
